f4a09a9276067338d659caed251dc15025f32a20
zenthomas <zensrss@gmail.com>
2015-01-24 12:59:32 +0800

    define movement

--- TheBomberMan/src/System/LivingObj.java
+++ TheBomberMan/src/System/LivingObj.java
@@ -2,9 +2,26 @@ package System;
 
 public abstract class LivingObj extends Units {
 
+	private final int moveInc = 32;
+	
 	public LivingObj(int x, int y) {
 		super(x, y);
 		// TODO Auto-generated constructor stub
 	}
 
+	public void moveUp() {
+		setY(getY() - moveInc);
+	}
+	
+	public void moveDown() {
+		setY(getY() + moveInc);
+	}
+	
+	public void moveLeft(){
+		setX(getX() - moveInc);
+	}
+	
+	public void moveRight(){
+		setX(getX() + moveInc);
+	}
 }
