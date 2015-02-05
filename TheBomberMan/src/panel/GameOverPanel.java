dd72c4089e735cbf00f51db4abdf586e8c1ea187
zenthomas <zensrss@gmail.com>
2015-02-06 00:47:11 +0800

    GameOverPanel

---
+++ TheBomberMan/src/panel/GameOverPanel.java
@@ -0,0 +1,32 @@
+package panel;
+
+import System.GraphicEngine;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+
+public class GameOverPanel extends Canvas{
+	Image image = new Image("Image/GameOver.png", 544.0, 416.0, false, true);
+	Button restart;
+	
+	public GameOverPanel(double width, double height) {
+		super(width, height);
+		this.getGraphicsContext2D().drawImage(image, 0, 0);
+		restart = new Button("Restart!");
+		restart.setLayoutX(450);
+		restart.setLayoutY(400);
+		restart.setOnAction(new EventHandler<ActionEvent>(){
+			
+			@Override
+			public void handle(ActionEvent arg0){
+				GraphicEngine.getRoot().getChildren().clear();
+				MainMenuPanel mainMenuPanel = new MainMenuPanel(Window.WIDTH, Window.HEIGHT);
+				GraphicEngine.getRoot().getChildren().add(mainMenuPanel);
+				GraphicEngine.getRoot().getChildren().add(mainMenuPanel.getHelpButton());
+				GraphicEngine.getRoot().getChildren().add(mainMenuPanel.getStartButton());
+			}
+		});
+	}
+}
